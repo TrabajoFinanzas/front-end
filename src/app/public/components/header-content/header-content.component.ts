6f62ca324b29dfbf358d6bc251494bfb33cfb0fb
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-05-21 14:25:03 -0500

    added platform & public folders

---
+++ src/app/public/components/header-content/header-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-content.component.html',
+  styleUrl: './header-content.component.css'
+})
+export class HeaderContentComponent {
+
+}
